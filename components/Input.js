22125a3a54bbed20cce27a3346b07f3328993824
Naveen Rawat <naveen.rawat@sprint.com>
2020-11-10 08:24:56 +0530

    screen added

---
+++ components/Input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+export default function Input(props) {
+
+    return(
+        <TextInput
+        {...props}
+        style={{
+            ...styles.input,
+            ...props.style}}
+            placeholder="Number"
+            autoCorrect={false}
+            keyboardType="numeric"
+            maxLength={2}/>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+        marginVertical: 10
+    }
+})
--- screens/startGameScreen.js
+++ screens/startGameScreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 export default function StartGameScreen() {
 
     return (
         <View style={styles.screen}>
-            <Text>The Number Guissing App</Text>
+            <Text style={styles.title}>Start a New App</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <Input style={styles.input} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.btn}>
+                    <Button title="Reset" color={Colors.accent} />
+                    </View>
+                    <View style={styles.btn}>
+                    <Button title="Confirm" color={Colors.primary} />
+                    </View>
+                </View>
+            </Card>
         </View>
     )
 };
@@ -16,5 +31,27 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
 
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    btn: {
+        width: 100
+    },
+    input: {
+        width: 70,
+        textAlign: 'center'
     }
 })
